9e0a5ef5a3bab082edb80f860f8d0d55130a1a0e
Martín Aberastegue <martin@aberastegue.com.ar>
2022-07-13 15:21:22 +0200

    Create meli-related-searches.py

---
+++ meli-related-searches.py
@@ -0,0 +1,148 @@
+import streamlit as st
+st.set_page_config(
+    page_title="Mercado Libre Related Search Scraper by Martin Aberastegue",
+    page_icon="chart_with_upwards_trend",
+    layout="wide",
+)
+from streamlit_echarts import st_echarts
+import requests
+import json
+from user_agent2 import (generate_user_agent)
+import pandas as pd
+
+st.title("Mercado Libre Related Search Scraper")
+st.subheader("Get Related Searches from Mercado Libre")
+st.write(
+    "An app which visualises related searches from Mercado Libre")
+st.write(
+    "Little tool based on the eBay script by [@LeeFootSEO](https://twitter.com/LeeFootSEO)")
+st.write("")
+
+with st.form(key='columns_in_form_2'):
+    seedkwd = st.text_input('Keyword Semilla')
+    submitted = st.form_submit_button('Enviar')
+    mercado = st.selectbox(
+        'Elige el mercado/pais. Argentina (MLA), Mexico (MLM), Brasil (MLB), Uruguay (MLU), Chile (MLC), Colombia (MCO), Peru (MPE)',
+        ('MLA', 'MLM', 'MLB', 'MLU', 'MLC', 'MCO', 'MPE'))
+
+if submitted:
+    def getkwds(seed_keyword, market):
+      ua = generate_user_agent(navigator="chrome")
+      header = {'User-Agent': str(ua)}
+      getterms_url = "https://http2.mlstatic.com/resources/sites/" + market + "/autosuggest?showFilters=true&limit=6&api_version=2&q="
+      response = requests.get(getterms_url + seed_keyword, headers=header)
+      return response
+
+    source_kws = []
+    final_kws = []
+
+    for i in getkwds(seedkwd, mercado).json()['suggested_queries']:
+        print("> ",i['q'])
+        for o in getkwds(i['q'], mercado).json()['suggested_queries']:
+          print(" - ", o['q'])
+          for p in getkwds(o['q'], mercado).json()['suggested_queries']:
+            print("    - ", p['q'])
+            source_kws.append(o['q'])
+            final_kws.append(p['q'])
+
+    df = pd.DataFrame(None)
+    df['seed_keyword'] = source_kws
+    df['related_searches'] = final_kws
+
+    try:
+        df['related_searches'] = df['related_searches'].str.split(',')
+    except Exception:
+        st.info("Error: No Related Searches Were Found, Try a Broader Keyword!")
+        st.stop()
+
+    df = df.explode('related_searches').reset_index(drop=True)
+
+
+    def visualize_autocomplete(df_autocomplete_full):
+        df_autocomplete_full['Keyword'] = seed_keyword
+
+        for query in df_autocomplete_full['Keyword'].unique():
+            df_autocomplete_full = df_autocomplete_full[df_autocomplete_full['Keyword'] == query]
+            children_list = []
+            children_list_level_1 = []
+
+            for int_word in df_autocomplete_full['seed_keyword']:
+                q_lv1_line = {"name": int_word}
+                if not q_lv1_line in children_list_level_1:
+                    children_list_level_1.append(q_lv1_line)
+
+                children_list_level_2 = []
+
+                for query_2 in df_autocomplete_full[df_autocomplete_full['seed_keyword'] == int_word][
+                    'related_searches']:
+                    q_lv2_line = {"name": query_2}
+                    children_list_level_2.append(q_lv2_line)
+
+                level2_tree = {'name': int_word, 'children': children_list_level_2}
+
+                if not level2_tree in children_list:
+                    children_list.append(level2_tree)
+
+                tree = {'name': query, 'children': children_list}
+
+                opts = {
+                    "backgroundColor": "#F0F2F6",
+
+
+                    "title": {
+                        "x": 'center',
+                        "y": 'top',
+                        "top": "5%",
+
+                        "textStyle": {
+                            "fontSize": 22,
+
+                        },
+                        "subtextStyle": {
+                            "fontSize": 15,
+                            "color": '#2ec4b6',
+
+                        },
+                    },
+
+                    "series": [
+                        {
+                            "type": "tree",
+                            "data": [tree],
+                            "layout": "radial",
+                            "top": "10%",
+                            "left": "25%",
+                            "bottom": "5%",
+                            "right": "25%",
+                            "symbolSize": 20,
+                            "itemStyle": {
+                                "color": '#2ec4b6',
+                            },
+                            "label": {
+                                "fontSize": 14,
+                            },
+
+                            "expandAndCollapse": True,
+                            "animationDuration": 550,
+                            "animationDurationUpdate": 750,
+                        }
+                    ],
+                }
+            st.header(f"MeLi Related Searches for: {query}")
+            st.caption("Right mouse click to save as image.")
+            st_echarts(opts, key=query, height=1700)
+
+    # add download button
+    def convert_df(df):  # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return df.to_csv().encode('utf-8')
+
+    csv = convert_df(df)
+
+    st.download_button(
+        label="📥 Download your report!",
+        data=csv,
+        file_name='meli_related_searches.csv',
+        mime='text/csv', )
+
+    # visualisation
+    visualize_autocomplete(df)
